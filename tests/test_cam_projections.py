2ca4b59d126be7ef6f41efeac2cab868b07d7619
snototter <snototter@users.noreply.github.com>
2020-01-16 22:53:35 +0100

    projection utils tests

--- tests/test_cam_projections.py
+++ tests/test_cam_projections.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pytest
 from vito.cam_projections import dot, apply_transformation, \
-    apply_projection, P_from_K_R_t
+    apply_projection, P_from_K_R_t, project_world_to_image_K_Rt, \
+    project_world_to_image_K_R_t, project_world_to_image_K_R_C, \
+    project_world_to_image_with_distortion_K_Rt, \
+    get_groundplane_to_image_homography
 
 
 def test_dot():
@@ -64,16 +67,31 @@ def test_apply_projection():
     assert x.shape[0] == 2 and x.shape[1] == 1
     assert x[0] == pytest.approx(T[0, 3]/T[2, 3])
     assert x[1] == pytest.approx(T[1, 3]/T[2, 3])
+    # 3D-2D projection can also be done via:
+    y = project_world_to_image_K_Rt(np.eye(3, 3), T, np.zeros((3, 1)))
+    assert np.all(y == x)
+    z = project_world_to_image_K_R_t(np.eye(3, 3), T[:, :3], T[:, -1], np.zeros((3, 1)))
+    assert np.all(z == y)
+    no_distortion = project_world_to_image_with_distortion_K_Rt(
+        np.eye(3, 3), T, np.zeros((5, 1)), np.zeros((3, 1)))
+    assert np.all(no_distortion == z)
+
+    H = get_groundplane_to_image_homography(T)
+    assert H.shape[0] == 3 and H.shape[1] == 3
+    assert np.all(H[:, 0] == T[:, 0])
+    assert np.all(H[:, 1] == T[:, 1])
+    assert np.all(H[:, 2] == T[:, 3])
 
     # Divide by zero (NumPy should take care of this)
+    np.seterr(divide='ignore', invalid='ignore')
     x = apply_projection(np.random.rand(3, 4), np.zeros((4, 1)))
     assert np.all(np.isnan(x))
+
 
 def test_P():
     I = np.eye(3, 3)
     R = np.random.rand(3, 3)
     t = np.random.rand(3, 1).reshape((3, ))
-    print(t.shape)
     P = P_from_K_R_t(I, R, t)
     assert P.shape[0] == 3 and P.shape[1] == 4
     assert np.all(P == np.column_stack((R, t)))
@@ -87,4 +105,4 @@ def test_P():
 
 # project_world_to_image_K_Rt(K, Rt, world_pts)
 # project_world_to_image_K_R_t
-# project_world_to_image_K_R_C
+# TODO project_world_to_image_K_R_C
