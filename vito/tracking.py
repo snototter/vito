e29c8dd7e4e2a2a8924d5abfe33c00865db501cf
snototter <snototter@users.noreply.github.com>
2020-11-10 23:18:57 +0100

    remove copied det code

--- vito/tracking.py
+++ vito/tracking.py
@@ -1,40 +1,5 @@
 # import numpy as np
-from types import SimpleNamespace
-
-
-def iou(bbox1, bbox2):
-    """Compute intersection over union for the two bounding boxes given in rect representation: [left, top, width, height]."""
-    if bbox1 is None or bbox2 is None:
-        return 0.0
-    if isinstance(bbox1, BoundingBox):
-        bbox1 = bbox1.to_rect_repr()
-    if isinstance(bbox2, BoundingBox):
-        bbox2 = bbox2.to_rect_repr()
-    left1, top1, width1, height1 = bbox1
-    left2, top2, width2, height2 = bbox2
-    right1 = left1 + width1
-    right2 = left2 + width2
-    bottom1 = top1 + height1
-    bottom2 = top2 + height2
-    # Compute the coordinates of the intersection rectangle
-    left = max(left1, left2)
-    top = max(top1, top2)
-    right = min(right1, right2)
-    bottom = min(bottom1, bottom2)
-    # Abort early if we don't have a valid intersection rectangle
-    if right < left or bottom < top:
-        return 0.0
-    # The intersection of two axis-aligned bounding boxes is always an
-    # axis-aligned bounding box. Right and bottom coordinates are exclusive.
-    intersection_area = (right - left) * (bottom - top)
-    # Compute the area of both AABBs
-    bb1_area = width1 * height1
-    bb2_area = width2 * height2
-    # Area of the union
-    union_area = float(bb1_area + bb2_area - intersection_area)
-    if union_area > 0.0:
-        return intersection_area / union_area
-    return 0.0  # pragma: no cover
+# from types import SimpleNamespace
 
 
 class Target(object):
