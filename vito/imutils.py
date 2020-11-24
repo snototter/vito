0a3b60e3754bfabec6d7554ca1e05c7d4237b31b
snototter <snototter@users.noreply.github.com>
2020-11-24 18:40:28 +0100

    linting

--- vito/imutils.py
+++ vito/imutils.py
@@ -6,10 +6,14 @@
 # * imread/imsave should use tifffile for tiff files - supports multi-band images
 
 import io
+import logging
 import os
 import numpy as np
 from PIL import Image
 from PIL import ImageFilter
+
+# Use logging module (fallback to default print) to log imutils set up (e.g. OpenCV vs Pillow).
+__log_fx = print if len(logging.getLogger().handlers) == 0 else logging.getLogger().info
 
 
 def flip_layers(nparray):
@@ -38,14 +42,12 @@ def rgb2gray(nparray, is_bgr=False):
         return np.dot(nparray[..., :3], [0.1140, 0.5870, 0.2989]).astype(nparray.dtype)
     else:
         return np.dot(nparray[..., :3], [0.2989, 0.5870, 0.1140]).astype(nparray.dtype)
-    
 
-import logging
-__log_fx = print if len(logging.getLogger().handlers) == 0 else logging.getLogger().info 
+
 try:
     # Try to load OpenCV (in case you installed it in your workspace)
     import cv2
-    __log_fx("vito.imutils will use OpenCV to save images via 'imsave'.")
+    __log_fx("vito.imutils will use OpenCV to save images via 'imsave'.")  # pragma: no cover
 
     def imsave(filename, image, flip_channels=False):  # pragma: no cover
         """Store an image using OpenCV."""
@@ -56,7 +58,7 @@ try:
         else:
             cv2.imwrite(filename, image)
 except:
-    __log_fx("vito.imutils will use Pillow to save images via 'imsave' (OpenCV could not be loaded).")
+    __log_fx("vito.imutils will use Pillow to save images via 'imsave' (OpenCV could not be loaded).")  # pragma: no cover
     # Fall back to Pillow
     def imsave(filename, image, flip_channels=False):
         """Store an image using PIL/Pillow, optionally flipping layers, i.e. BGR -> RGB."""
